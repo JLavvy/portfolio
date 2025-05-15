92e9df3b502fb3458b83de4038ad5d2c2dba98c8
JLavvy <lavogajanyce@gmail.com>
2025-05-15 19:40:47 +0300

    first Commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <p className="text-3xl font-extrabold text-amber-300">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
